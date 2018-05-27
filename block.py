5b0f9d84407d22ed94887b2c6aa8ab5f0342424f
Eric Saunders <eric.saunders@gmail.com>
2018-05-27 13:41:36 -0700

    simple Block implementation

--- block.py
+++ block.py
@@ -10,5 +10,24 @@ May 2018
 import hashlib
 
 class Block(object):
-    def __init__(self):
-        pass
+    def __init__(self, idx, timestamp, data, prev_hash):
+        self.idx       = idx
+        self.timestamp = timestamp
+        self.data      = data
+        self.prev_hash = prev_hash
+        self.hash      = self.get_hash()
+
+
+    def get_hash(self):
+        '''Simple hash function to uniquely identify each block. The hash
+           includes the previous block so that the chain is immutable.'''
+
+        sha = hashlib.sha256()
+        sha.update(
+                    str(self.idx) +
+                    str(self.timestamp) + 
+                    str(self.data) + 
+                    str(self.prev_hash)
+                  )
+
+        return sha.hexdigest()
