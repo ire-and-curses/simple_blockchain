13789aad37846d89883d862a130b05c4d578c61e
Eric Saunders <eric.saunders@gmail.com>
2018-05-27 11:49:24 -0700

    created block skeleton

---
+++ block.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+block.py - Simple linked hashes for simulating basic blockchain concepts
+
+Author: Eric Saunders
+May 2018
+'''
+
+import hashlib
+
+class Block(object):
+    def __init__(self):
+        pass
